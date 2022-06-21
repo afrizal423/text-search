cac09e8516fcede50042b0194aca93ec9e2d96bb
afrizal423 <afrizal2499@gmail.com>
2022-06-21 10:19:47 +0700

    perhitungan manual
    changes:
    - update perhitungan idf + 1
    - add file excel perhitungan
    
    next:
    - jaccard

--- coba.php
+++ coba.php
@@ -3,23 +3,39 @@ require 'vendor/autoload.php';
 
 use Afrizalmy\TIJ\TfIdfJaccard;
 
+//helper 
+function array_insert(&$arr, $index, $val)
+{
+    if (is_string($index))
+        $index = array_search($index, array_keys($arr));
+    if (is_array($val))
+        array_splice($arr, $index, 0, [$index => $val]);
+    else
+        array_splice($arr, $index, 0, $val);
+}
+
+//soal
+// Apa yang kamu ketahui tentang perpustakaan?
+// jawaban
 // string dokumen (dijadikan array)
-// $str = array(
-//     "Perpustakaan adalah tempat membaca.😀",
-//     "Ada banyak sekali buku cerita di perpustakaan.😃",
-//     "Membaca buku ketika waktu luang adalah kebiasaan baik!.😄",
-//     'Tetap berhati-hati ya, menghadapi resesi harga jual buku-buku semakin mahal. Terburu-buru'
-// );
 $str = array(
-    "Program logika dan semantik",
-    "ilmu antar individu",
-    "Dalam program ilmu terdapat transfer ilmu semantik"
+    "Perpustakaan adalah tempat membaca buku.",
+    "Ada banyak sekali buku di perpustakaan.",
+    "perpustakaan tempat nyaman buat belajar",
 );
+// $str = array(
+//     "Program logika dan semantik",
+//     "ilmu antar individu",
+//     "Dalam program ilmu terdapat transfer ilmu semantik"
+// );
 
 // query tetep satu, namun tetap berupa array saja
-$query = array(
-    "Sekumpulan instruksi-instruksi yang dijalankan oleh komputer untuk menjalankan fungsi"
-);
+$query = "perpustakaan adalah tempat paling enak buat membaca dan banyak buku";
+
+array_insert($str, 0, $query);
+// array_push($str,$query);
+// var_dump($str);
+// array_push($arr,$str);
 //pemanggilan
 
 $tfidfjaccard = new TfIDFJaccard();
